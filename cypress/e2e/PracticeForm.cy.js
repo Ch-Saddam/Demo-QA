2459599e8ef1ed7099b932d983e1d76ce0493c43
Chaudhary Saddam Hussain <chaudharysaddam0011@gmail.com>
2023-09-13 11:23:28 +0500

    With-fixes

--- cypress/e2e/PracticeForm.cy.js
+++ cypress/e2e/PracticeForm.cy.js
@@ -6,12 +6,28 @@ describe('Automating Practice Form', () => {
     cy.contains("Practice Form").click();     // Tap on practice form
 
     // Form Filling
+const date = new Date('2023-09-12');
+
+
+const options = {
+  year: 'numeric',  
+  month: 'long',    
+  day: 'numeric',   
+};
+
+// Format the date using toLocaleDateString with options
+const formattedDate = date.toLocaleDateString('en-NZ', options);
+
     cy.get("#firstName").type("Cowlar");
     cy.get("#lastName").type("Developer");
     cy.get("#userEmail").type("qaengineer@cowlar.com");
     cy.get("[for='gender-radio-1']").click(); // Select Male
     cy.get("#userNumber").type("0123456789");
     // Date of Birth is kept default
+    cy.get("#dateOfBirthInput").invoke("val").should("not.be.empty").then(defaultDate => {
+      // Log the default date
+      cy.log(`Default Date: ${defaultDate}`);
+    });
     cy.get("#subjectsInput").type("Computer Science").type("{enter}");
     cy.get("[for='hobbies-checkbox-3']").click(); // Select Music
     cy.get("#currentAddress").type("Address 1");
@@ -21,24 +37,38 @@ describe('Automating Practice Form', () => {
     cy.get("#city").click().type("Delhi").click().type("{enter}")
 
     // Click on the Submit button
-    cy.get("#submit").click({ force: true });
+    cy.get("#submit").then($button => {
+      // Scroll to the button to make sure it's visible
+      cy.wrap($button).scrollIntoView();
+      $button[0].click();
+    });
 
     // Verify the information in the modal
-    cy.get(".modal-content").should("contain.text", "Cowlar Developer");
-    cy.get(".modal-content").should("contain.text", "qaengineer@cowlar.com");
-    cy.get(".modal-content").should("contain.text", "Male");
-    cy.get(".modal-content").should("contain.text", "0123456789");
-    cy.get(".modal-content").should("contain.text", "Computer Science");
-    cy.get(".modal-content").should("contain.text", "Music");
-    cy.get(".modal-content").should("contain.text", "Address 1");
-    cy.get(".modal-content").should("contain.text", "NCR Delhi");
+    cy.get(".modal-content").should("contain.text", "Cowlar Developer")
+      .should("contain.text", "qaengineer@cowlar.com")
+      .should("contain.text", "Male")
+      .should("contain.text", "0123456789")
+      // The date can modal is display with comma after the month, so can no
+      .should("contain.text", formattedDate )
+      .should("contain.text", "Computer Science")
+      .should("contain.text", "Music")
+      .should("contain.text", "Address 1")
+      .should("contain.text", "NCR Delhi");
 
     // Close the Modal
     cy.get('#example-modal-sizes-title-lg').should('contain', 'Thanks for submitting the form')
-    cy.get("#closeLargeModal").click({ force: true });
+
+    //cy.get("#closeLargeModal").scrollIntoView().click();
+    cy.get("#closeLargeModal").then($button => {
+      // Scroll to the button to make sure it's visible
+      cy.wrap($button).scrollIntoView();
+
+      // Trigger a click using JavaScript
+      $button[0].click();
+
+    })
   })
 })
-
 
 
 
---
+++ cypress/e2e/bookpractice.cy.js
@@ -0,0 +1,48 @@
+it('Intercept by RouteMatcher ', () => {
+  cy.visit("https://demoqa.com/");
+
+  // Click on Book Store 
+  cy.contains("Book Store Application").click();
+
+  // Click on Book Store Tab
+  cy.contains("Book Store").click();
+
+  // Click on the Understanding ECMAScript 6 book
+  cy.contains("Understanding ECMAScript 6").click();
+  cy.intercept({
+    method: 'GET',
+    url: 'https://demoqa.com/BookStore/v1/Book?ISBN=9781593277574 '
+  },
+    (req) => {
+      req.reply({
+        body: {
+          isbn: "9781593277574",
+          title: "Understanding ECMAScript 6",
+          subTitle: "The Definitive Guide for JavaScript Developers",
+          author: "Nicholas C. Zakas",
+          publish_date: "2016-09-03T00:00:00.000Z",
+          publisher: "No Starch Press",
+          pages: 352,
+          description:
+            "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that E",
+          website: "https://leanpub.com/understandinges6/read",
+        }
+      })
+    }).as('getdata')
+  cy.visit('https://demoqa.com/books?book=9781593277574')
+  cy.wait('@getdata')
+  cy.get('@getdata').then((interception => {
+    expect(interception.response.body).to.deep.equal({
+      isbn: "9781593277574",
+      title: "Understanding ECMAScript 6",
+      subTitle: "The Definitive Guide for JavaScript Developers",
+      author: "Nicholas C. Zakas",
+      publish_date: "2016-09-03T00:00:00.000Z",
+      publisher: "No Starch Press",
+      pages: 352,
+      description: "ECMAScript 6 represents the biggest update to the core of JavaScript in the history of the language. In Understanding ECMAScript 6, expert developer Nicholas C. Zakas provides a complete guide to the object types, syntax, and other exciting changes that E",
+      website: "https://leanpub.com/understandinges6/read"
+    });
+  }))
+
+})
